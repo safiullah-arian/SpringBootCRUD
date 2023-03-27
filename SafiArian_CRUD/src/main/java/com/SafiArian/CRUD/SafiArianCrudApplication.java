79e4cab70a10b592bfd037292be533a5871d3945
Safiullah Arian <ahsas.a246@gmail.com>
2023-03-28 00:35:03 +0430

    new CRUD

---
+++ SafiArian_CRUD/src/main/java/com/SafiArian/CRUD/SafiArianCrudApplication.java
@@ -0,0 +1,13 @@
+package com.SafiArian.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafiArianCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafiArianCrudApplication.class, args);
+	}
+
+}
---
+++ SafiArian_CRUD/src/test/java/com/SafiArian/CRUD/SafiArianCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SafiArian.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafiArianCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
